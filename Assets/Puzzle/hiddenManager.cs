a3220c095db85718b2bc55f1dca932b1351e3869
FazaIsThe-Nine <faza6dilan@gmail.com>
2023-05-25 12:06:38 +0700

    Puzzle Finish
    Tinggal di gabungin ke main

--- Assets/Puzzle/hiddenManager.cs
+++ Assets/Puzzle/hiddenManager.cs
@@ -1,13 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class hiddenManager : MonoBehaviour
 {
     public GameObject item;
+    public GameObject[] itemTarget;
+    public int[] randomIndexs;
+    public GameObject panelFinish;
+    int countItemFind;
+    public int countHealth;
+    public GameObject health;
+    public TMP_Text textGagal;
     private void Start() 
     {
         RandomItemPos();
+
+        RandomIndex();
+
+        RandomItemTarget();
+    }
+    void RandomIndex()
+    {
+        for(int i = 0; i < randomIndexs.Length; i++) 
+        {
+            int a = randomIndexs[i];
+            int b = Random.Range(0, randomIndexs.Length);
+            randomIndexs[i] = randomIndexs[b];
+            randomIndexs[b] = a;
+        }
+    }
+    void RandomItemTarget()
+    {
+        for(int i = 0; i < itemTarget.Length; i++) 
+        {
+            itemTarget[i].GetComponent<Image>().sprite = item.transform.GetChild(randomIndexs[i]).GetComponent<Image>().sprite;
+        }
     }
     public void RandomItemPos() 
     {
@@ -19,5 +51,48 @@ public class hiddenManager : MonoBehaviour
 
             item.transform.GetChild(i).gameObject.SetActive(true);
         }
+    }
+    public void ButtonItem() 
+    {
+        for(int i = 0; i < itemTarget.Length; i++) 
+        {    
+            if(EventSystem.current.currentSelectedGameObject.GetComponent<Image>().sprite == itemTarget[i].GetComponent<Image>().sprite)
+            {
+                itemTarget[i].GetComponent<Image>().color = Color.white;
+
+                countItemFind++;
+                Debug.Log("benar " + countItemFind);
+                if (countItemFind >= itemTarget.Length)
+                {
+                    panelFinish.SetActive(true);
+                }
+
+                EventSystem.current.currentSelectedGameObject.gameObject.SetActive(false);
+                return;
+            }
+            else
+            {
+                if(i == itemTarget.Length - 1)
+                {
+                    Debug.Log("salah");
+                    if(countHealth > 0) {
+
+                        health.transform.GetChild(countHealth - 1).gameObject.SetActive(false);
+                        countHealth -= 1;
+                    }
+                    else
+                    {
+                        panelFinish.SetActive(true);
+                        textGagal.text = "Misi Gagal";
+                    }
+                }
+            }
+            
+        }
+    }
+
+    public void Ulangi () 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
