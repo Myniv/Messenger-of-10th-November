e21b718c8f08bc88344d6abc374bbcbeba6d031a
mulyanan26 <mulyanan.ur26@gmail.com>
2023-05-26 10:57:24 +0700

    Try to fix bug with cinemachine

--- Assets/Script/Character Script/Character.cs
+++ Assets/Script/Character Script/Character.cs
@@ -40,6 +40,7 @@ public class Character : MonoBehaviour
         button.enabled=true;
 
         if(other.CompareTag("NPC")){
+            Debug.Log("TESTT");
             enterNPC.Invoke();
         }
     } 
