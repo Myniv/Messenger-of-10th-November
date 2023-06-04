336eeec15182d3ea92657ccc9d8b8b247c0aa9a9
rifkigilangs <72380161+rifkigilangs@users.noreply.github.com>
2023-06-04 14:10:38 +0700

    p

---
+++ Assets/Script/NPCItem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NPCItem : MonoBehaviour
+{
+    [SerializeField] GameObject nextPanel;
+    [SerializeField] TMP_Text confirmText;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            //playerIsClose = true;
+            nextPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            //playerIsClose = false;
+            nextPanel.SetActive(value: false);
+        }
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Level 1.1.1");
+    }
+}
