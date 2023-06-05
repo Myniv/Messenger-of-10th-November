ea5be1d4e1d3a33695f7167fa2c67ec88faf0aaf
mulyanan26 <mulyanan.ur26@gmail.com>
2023-06-05 20:51:43 +0700

    Add Fiture Inventory Achievment

--- Assets/Script/AchievmentScript/AchievementButton.cs
+++ Assets/Script/AchievmentScript/AchievementButton.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 public class AchievementButton : MonoBehaviour
 {
     public GameObject achievmentList;
+
     [SerializeField] Sprite  neutral, highlight;
 
     private Image sprite;
 
+    [SerializeField] Button noteButton;
+
     private void Awake() {
         sprite = GetComponent<Image>();
+        noteButton.interactable=false;
     }
+    
     public void Click(){
         if(sprite.sprite == highlight){
             sprite.sprite = neutral;
--- Assets/Script/AchievmentScript/AchievementManager.cs
+++ Assets/Script/AchievmentScript/AchievementManager.cs
@@ -6,30 +6,31 @@ using UnityEngine.UI;
 
 public class AchievementManager : MonoBehaviour
 {
-    [SerializeField] GameObject achievmentPrefab;
-
-    [SerializeField] Sprite[] sprites;
-
     [SerializeField] ScrollRect scrollRect;
     private AchievementButton activeButton;
+    public GameObject AchievmentMenu;
+
+    public GameObject visualAchievment;
+
+    int indexAchievementButton;
+
+    public Dictionary<string, Achievment> achievments = new Dictionary<string, Achievment>();
+    [SerializeField] List<Button> achievementButtons;
+    [SerializeField] Sprite[] sprites;
+
+    public List<AchievmentData> achievmentData;
 
 
     void Start()
     {
         activeButton = GameObject.Find("GeneralBtn").GetComponent<AchievementButton>();
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
-        CreateAchievment("General", "TestTitle", "This is Description", 0);
+        achievementButtons[0].interactable=true;
 
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
-        CreateAchievment("Other", "TestTitleOther", "This is Description", 0);
+        foreach (var arch in achievmentData)
+        {
+            CreateAchievment("other", arch.title, arch.description, arch.spriteIndex);
+        }
+
 
         foreach (GameObject achievmentList in GameObject.FindGameObjectsWithTag("AchievmentList"))
         {
@@ -37,24 +38,65 @@ public class AchievementManager : MonoBehaviour
         }
 
         activeButton.Click();
+
+        AchievmentMenu.SetActive(false);
+
+        CheckPlayerPrefsAchievment();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            AchievmentMenu.SetActive(!AchievmentMenu.activeSelf);
+        }
+    }
+
+    //Perlu
+    public void EarnAchievment(string title)
+    {
+        if (achievments[title].EarnAchievment())
+        {
+            GameObject achievment = (GameObject)Instantiate(visualAchievment);
+            SetAchievmnetInfo("EarnCanvas", achievment, title);
+
+            StartCoroutine(HideAchievment(achievment));
+
+            CheckPlayerPrefsAchievment();
+        }
+    }
+
+    //Perlu
+    public IEnumerator HideAchievment(GameObject achievment)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(achievment);
     }
 
 
-    public void CreateAchievment(string category, string title, string description, int spriteIndex)
+    public void CreateAchievment(string parent, string title, string description, int spriteIndex)
     {
-        GameObject achievment = (GameObject)Instantiate(achievmentPrefab);
-        SetAchievmnetInfo(category, achievment, title, description, spriteIndex);
+        // GameObject achievment = (GameObject)Instantiate(visualAchievment);
+
+        Achievment newAchievment = new Achievment(name, description, spriteIndex);
+
+        achievments.Add(title, newAchievment);
+
     }
 
-    public void SetAchievmnetInfo(string category, GameObject achievment, string title, string description, int spriteIndex)
+    //Untuk membuat notifikasi achievment
+    public void SetAchievmnetInfo(string parent, GameObject achievment, string title)
+    // , string description, int points, int spriteIndex
     {
-        achievment.transform.SetParent(GameObject.Find(category).transform);
+        achievment.transform.SetParent(GameObject.Find(parent).transform);
         achievment.transform.localScale = new Vector3(1, 1, 1);
         achievment.transform.GetChild(0).GetComponent<TMP_Text>().text = title; //Title
-        achievment.transform.GetChild(1).GetComponent<TMP_Text>().text = description; //description
-        achievment.transform.GetChild(2).GetComponent<Image>().sprite = sprites[spriteIndex]; //sprite
+        achievment.transform.GetChild(1).GetComponent<TMP_Text>().text = achievments[title].Description; //description
+        achievment.transform.GetChild(3).GetComponent<Image>().sprite = sprites[achievments[title].SpriteIndex]; //sprite
     }
 
+
+    //Untuk menggani category didalam canvas
     public void ChangeCategory(AchievementButton button)
     {
         AchievementButton achievementButton = button.GetComponent<AchievementButton>();
@@ -63,5 +105,23 @@ public class AchievementManager : MonoBehaviour
         achievementButton.Click();
         activeButton.Click();
         activeButton = achievementButton;
+    }
+
+    public void EnabledButtonAchievment(int index)
+    {
+        PlayerPrefs.SetInt($"Achievment{index}", 1);
+
+    }
+
+    private void CheckPlayerPrefsAchievment()
+    {
+        for (int i = 1; i < achievementButtons.Count; i++)
+        {
+            var unlock = PlayerPrefs.GetInt($"Achievment{i}");
+            if (unlock == 1)
+            {
+                achievementButtons[i].interactable = true;
+            }
+        }
     }
 }
