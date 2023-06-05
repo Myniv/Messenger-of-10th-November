2f42a7ad1782f2b8f72e196c5608e43d89e565a1
FazaIsThe-Nine <faza6dilan@gmail.com>
2023-06-05 13:51:09 +0700

    audio

--- Assets/Script/pengaturan.cs
+++ Assets/Script/pengaturan.cs
@@ -1,38 +1,51 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-// public class pengaturan : MonoBehaviour
-// {
-//     [SerializeField] Slider volumeSlider;
-//     void Start()
-//     {
-//         if(PlayerPrefs.HasKey("musicVolume"))
-//         {
-//             PlayerPrefs.SetFloat("musicVolume", 1);
-//             Load();
-//         }
-//         else
-//         {
-//             Load();
-//         }
-//     }
+public class pengaturan : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider SFXSlider;
 
-//     // Update is called once per frame
-//     public void ChangeVolume()
-//     {
-//         AudioListener.volume = volumeSlider.value;
-//         Save();
-//     }
+    private void Start() 
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetBgmVolume();
+            SetSFXVolume();
+            SetMute();
+        }
+    }
+    public void SetBgmVolume () 
+    {
+        float volume = volumeSlider.value;
+        myMixer.SetFloat("BGM", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume () 
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    public void SetMute () 
+    {
+        AudioManager.instance.Mute();
+    }
+     private void LoadVolume()
+     {
+         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+         SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+         SetBgmVolume();
+         SetSFXVolume();
+         SetMute();
+     }
 
-//     private void Load()
-//     {
-//         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
-//     }
-
-//     private void Save()
-//     {
-//         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
-//     }
-// }
+}
