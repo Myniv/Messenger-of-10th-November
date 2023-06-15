7f38f9dd52c65ee831e4e2aebc2887348badde69
FazaIsThe-Nine <faza6dilan@gmail.com>
2023-06-15 23:12:52 +0700

    update footstep

--- Assets/Script/Character Script/RightMove.cs
+++ Assets/Script/Character Script/RightMove.cs
@@ -7,19 +7,24 @@ public class RightMove : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
     [SerializeField] Character character;
+    AudioManager audioManager;
     bool isPressed = false;
     bool m_FacingRight = true;
-
+    private void Start() {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     void Update()
     {
         if (isPressed)
         {
             if (character.Corner == true)
             {
+                audioManager.PlaySFX(audioManager.footstep);
                 character.transform.Translate(0 * Time.deltaTime, 0, 0);
             }
             else
             {
+                audioManager.PlaySFX(audioManager.footstep);
                 character.transform.Translate(character.force * Time.deltaTime, 0, 0);
             }
         }
